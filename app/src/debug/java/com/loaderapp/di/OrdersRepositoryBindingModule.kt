5c5211cce445c59c7323d2c8d8a544ada0b322d9
Foge1 <100696836+Foge1@users.noreply.github.com>
2026-02-25 18:09:06 +0300

    Move FakeOrdersRepository to debug and align OrdersRepository bindings

--- app/src/debug/java/com/loaderapp/di/OrdersRepositoryBindingModule.kt
+++ app/src/debug/java/com/loaderapp/di/OrdersRepositoryBindingModule.kt
@@ -2,17 +2,19 @@ package com.loaderapp.di
 
 import com.loaderapp.features.orders.data.FakeOrdersRepository
 import com.loaderapp.features.orders.domain.repository.OrdersRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object OrdersRepositoryBindingModule {
+abstract class OrdersRepositoryBindingModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideOrdersRepository(): OrdersRepository = FakeOrdersRepository()
+    abstract fun bindOrdersRepository(
+        impl: FakeOrdersRepository
+    ): OrdersRepository
 }
