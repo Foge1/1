7d5cdfd13c14c4fcd42eefe4f7ed54cb4fe11a04
Foge1 <100696836+Foge1@users.noreply.github.com>
2026-03-09 05:50:21 +0300

    Reorder import statements in SegmentedTabs.kt

--- core-ui/src/main/java/com/loaderapp/core/ui/components/SegmentedTabs.kt
+++ core-ui/src/main/java/com/loaderapp/core/ui/components/SegmentedTabs.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.weight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Text
@@ -19,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
